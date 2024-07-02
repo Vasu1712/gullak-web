f7b883d441c81db0182dedef65ff58f650a51e9c
Vasu1712 <vasu.pal.ug20@nsut.ac.in>
2024-07-02 23:04:32 +0530

    fixed goals

--- client/src/routes/Statistics.js
+++ client/src/routes/Statistics.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Icon } from '@iconify/react';
 import Chart from 'react-apexcharts';
@@ -6,6 +6,22 @@ import profileimage from '../assets/images/profile_image.png';
 import Footer from './Footer';
 
 const Statistics = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [newGoal, setNewGoal] = useState({
+    title: '',
+    description: '',
+    targetAmount: '',
+    progress: 0,
+  });
+  const [goals, setGoals] = useState([
+    {
+      title: 'iPad 15',
+      description: 'Goal is in progress',
+      targetAmount: '₹27590',
+      progress: 52.1,
+    },
+  ]);
+
   const chartOptions = {
     chart: {
       type: 'area',
@@ -30,7 +46,7 @@ const Statistics = () => {
       enabled: true,
     },
     grid: {
-      show: false, // Hide the grid lines
+      show: false,
     },
     colors: ['#8E7AF7'],
   };
@@ -84,6 +100,31 @@ const Statistics = () => {
 
   const radialChartSeries = [52.1];
 
+  const handlePlusClick = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNewGoal({ ...newGoal, [name]: value });
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    setGoals([...goals, { ...newGoal, progress: 0 }]);
+    setNewGoal({
+      title: '',
+      description: '',
+      targetAmount: '',
+      progress: 0,
+    });
+    setIsModalOpen(false);
+  };
+
   return (
     <div className="min-h-screen bg-gradient-to-br from-grad1 via-grad2 to-grad3 p-4">
       <div className="flex justify-between items-center">
@@ -114,37 +155,86 @@ const Statistics = () => {
           <Chart options={chartOptions} series={chartSeries} type="area" height={240} />
         </div>
       </div>
-      <Link to="/overview" >
+      <Link to="/overview">
         <div className="flex pt-3 px-4 text-sm text-blue-600 italic font-semibold justify-end items-center">
             see more
         </div>
       </Link>
       <div className="mt-2 w-full bg-inherit p-4 rounded-xl mb-4">
         <h2 className="text-lg font-semibold mb-2">Goals</h2>
-        <div className="flex items-center justify-between pt-2 bg-violet-200 drop-shadow-md rounded-xl mb-4">
-          <div className="flex flex-col px-6 py-2">
-            <h3 className="text-deepblue2 text-xl font-bold">iPad 15</h3>
-            <p className="text-sm text-gray-500 italic">Goal is in progress</p>
-            <p className="text-sm text-gray-700 pt-4">
-              Target amount left: <span className="text-darkpurple font-semibold">₹27590</span>
-            </p>
+        {goals.map((goal, index) => (
+          <div key={index} className="flex items-center justify-between pt-2 bg-violet-200 drop-shadow-md rounded-xl mb-4">
+            <div className="flex flex-col px-6 py-2">
+              <h3 className="text-deepblue2 text-xl font-bold">{goal.title}</h3>
+              <p className="text-sm text-gray-500 italic">{goal.description}</p>
+              <p className="text-sm text-gray-700 pt-4">
+                Target amount left: <span className="text-darkpurple font-semibold">{goal.targetAmount}</span>
+              </p>
+            </div>
+            <div className="relative w-36 h-36">
+              <Chart options={radialChartOptions} series={[goal.progress]} type="radialBar" height="100%" />
+            </div>
           </div>
-          <div className="relative w-36 h-36">
-            <Chart options={radialChartOptions} series={radialChartSeries} type="radialBar" height="100%" />
-          </div>
-        </div>
+        ))}
         <div className="flex items-center justify-between p-4 bg-violet-200 rounded-xl">
           <div className="flex flex-col">
             <h3 className="text-deepblue2 font-bold">Create a new Goal</h3>
             <p className="text-sm text-gray-500">Reach more Goals</p>
           </div>
-          <button className="bg-gradient-to-br from-purple-500 to-purple-700 text-white p-2 rounded-full shadow-lg">
+          <button className="bg-gradient-to-br from-purple-500 to-purple-700 text-white p-2 rounded-full shadow-lg" onClick={handlePlusClick}>
             <Icon icon="mdi:plus" width="24" />
           </button>
         </div>
       </div>
 
       <Footer />
+
+      {isModalOpen && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+          <div className="bg-white p-8 rounded-lg shadow-lg">
+            <h2 className="text-xl font-bold mb-4">Create a New Goal</h2>
+            <form onSubmit={handleFormSubmit}>
+              <div className="mb-4">
+                <label className="block text-gray-700">Title</label>
+                <input
+                  type="text"
+                  name="title"
+                  value={newGoal.title}
+                  onChange={handleInputChange}
+                  className="w-full p-2 border border-gray-300 rounded"
+                  required
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-gray-700">Description</label>
+                <input
+                  type="text"
+                  name="description"
+                  value={newGoal.description}
+                  onChange={handleInputChange}
+                  className="w-full p-2 border border-gray-300 rounded"
+                  required
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-gray-700">Target Amount</label>
+                <input
+                  type="text"
+                  name="targetAmount"
+                  value={newGoal.targetAmount}
+                  onChange={handleInputChange}
+                  className="w-full p-2 border border-gray-300 rounded"
+                  required
+                />
+              </div>
+              <div className="flex justify-end">
+                <button type="button" onClick={handleModalClose} className="bg-gray-500 text-white px-4 py-2 rounded mr-2">Cancel</button>
+                <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">Create</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- client/src/routes/Transactions.js
+++ client/src/routes/Transactions.js
@@ -1,100 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Footer from './Footer';
 
 const Transactions = () => {
+  const [transactions, setTransactions] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios.get('https://gullak-425421.uc.r.appspot.com/gmail-data')
+      .then((response) => {
+        setTransactions(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  const formatDate = (dateString) => {
+    const options = { year: 'numeric', month: 'long', day: '2-digit' };
+    return new Date(dateString).toLocaleDateString('en-GB', options);
+  };
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
   return (
     <div className="bg-gradient-to-br from-grad1 via-grad2 to-grad3 p-4">
-        <div className="mt-6 w-full overflow-y-auto mb-4">
-            <h4 className="text-3xl font-semibold text-gray-800 mb-6">Transaction History</h4>
-            <ul className="space-y-2">
-            <li className="flex justify-between items-center bg-white px-4 py-2 rounded-xl">
-                <div>
-                <h5 className="font-md text-lg">Puran's Store</h5>
-                <p className="text-sm text-gray-500">02 July 2023 | 10:00</p>
-                </div>
-                <p className="text-red-500">₹202</p>
+      <div className="mt-6 w-full overflow-y-auto mb-4">
+        <h4 className="text-3xl font-semibold text-gray-800 mb-6">Transaction History</h4>
+        <ul className="space-y-2">
+          {transactions.map((transaction, index) => (
+            <li key={index} className="flex justify-between items-center bg-white px-4 py-2 rounded-xl">
+              <div className="flex-grow">
+                <h5 className="font-md text-lg">{transaction.paid_to}</h5>
+                <p className="text-sm text-gray-500">{formatDate(transaction.received_date)} | {transaction.received_time}</p>
+              </div>
+              <p className={`text-red-500`}>₹{transaction.amount}</p>
             </li>
-            <li className="flex justify-between items-center bg-white px-4 py-2 rounded-xl">
-                <div>
-                <h5 className="font-md text-lg">Excitel Wi-Fi Bill</h5>
-                <p className="text-sm text-gray-500">29 June 2023 | 14:40</p>
-                </div>
-                <p className="text-red-500">₹120</p>
-            </li>
-            <li className="flex justify-between items-center bg-white px-4 py-2 rounded-xl">
-                <div>
-                <h5 className="font-md text-lg">Stipend</h5>
-                <p className="text-sm text-gray-500">24 June 2023 | 12:10</p>
-                </div>
-                <p className="text-green-500">₹100000</p>
-            </li>
-            <li className="flex justify-between items-center bg-white px-4 py-2 rounded-xl">
-                <div>
-                <h5 className="font-md text-lg">Monthly Subscription</h5>
-                <p className="text-sm text-gray-500">22 June 2023 | 10:00</p>
-                </div>
-                <p className="text-red-500">₹699</p>
-            </li>
-            <li className="flex justify-between items-center bg-white px-4 py-2 rounded-xl">
-                <div>
-                <h5 className="font-md text-lg">Puran's Store</h5>
-                <p className="text-sm text-gray-500">02 July 2023 | 10:00</p>
-                </div>
-                <p className="text-red-500">₹202</p>
-            </li>
-            <li className="flex justify-between items-center bg-white px-4 py-2 rounded-xl">
-                <div>
-                <h5 className="font-md text-lg">Excitel Wi-Fi Bill</h5>
-                <p className="text-sm text-gray-500">29 June 2023 | 14:40</p>
-                </div>
-                <p className="text-red-500">₹120</p>
-            </li>
-            <li className="flex justify-between items-center bg-white px-4 py-2 rounded-xl">
-                <div>
-                <h5 className="font-md text-lg">Stipend</h5>
-                <p className="text-sm text-gray-500">24 June 2023 | 12:10</p>
-                </div>
-                <p className="text-green-500">₹100000</p>
-            </li>
-            <li className="flex justify-between items-center bg-white px-4 py-2 rounded-xl">
-                <div>
-                <h5 className="font-md text-lg">Monthly Subscription</h5>
-                <p className="text-sm text-gray-500">22 June 2023 | 10:00</p>
-                </div>
-                <p className="text-red-500">₹699</p>
-            </li>
-            <li className="flex justify-between items-center bg-white px-4 py-2 rounded-xl">
-                <div>
-                <h5 className="font-md text-lg">Puran's Store</h5>
-                <p className="text-sm text-gray-500">02 July 2023 | 10:00</p>
-                </div>
-                <p className="text-red-500">₹202</p>
-            </li>
-            <li className="flex justify-between items-center bg-white px-4 py-2 rounded-xl">
-                <div>
-                <h5 className="font-md text-lg">Excitel Wi-Fi Bill</h5>
-                <p className="text-sm text-gray-500">29 June 2023 | 14:40</p>
-                </div>
-                <p className="text-red-500">₹120</p>
-            </li>
-            <li className="flex justify-between items-center bg-white px-4 py-2 rounded-xl">
-                <div>
-                <h5 className="font-md text-lg">Stipend</h5>
-                <p className="text-sm text-gray-500">24 June 2023 | 12:10</p>
-                </div>
-                <p className="text-green-500">₹100000</p>
-            </li>
-            <li className="flex justify-between items-center bg-white px-4 py-2 rounded-xl">
-                <div>
-                <h5 className="font-md text-lg">Monthly Subscription</h5>
-                <p className="text-sm text-gray-500">22 June 2023 | 10:00</p>
-                </div>
-                <p className="text-red-500">₹699</p>
-            </li>
-            </ul>
-        </div>
-        <Footer/>
-    </div>    
+          ))}
+        </ul>
+      </div>
+      <Footer />
+    </div>
   );
 };
 
